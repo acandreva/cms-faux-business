fc2b558382a071ae5987e96b558eacf552311295
Allison Logan <ali.cand@yahoo.com>
2015-09-13 12:24:20 -0400

    updates search file and adds search form

--- search.php
+++ search.php
@@ -2,8 +2,9 @@
 /*Template Name: Search Page*/
 
 get_header(); ?>
+<div class="container marginspage">
     <div class="row">
-        <div class="nine columns">
+        <div class="eight columns">
             <h2><?php printf(__('Search results for: %s'), '<span>' . get_search_query() . '</span>' ); ?></h2>
             <?php if (have_posts()) : 
                 while (have_posts()) : the_post(); ?> 
@@ -15,9 +16,13 @@ get_header(); ?>
             <p>Sorry, nothing matched your search criteria. Please try again with different search terms.</p>
             <?php endif; ?>
         </div>
-        <div class="three columns">
+        <div class="four columns sidebarstyle">
+            <div class="searchbx">
+                <?php get_search_form(); ?>
+            </div>
             <?php get_sidebar(); ?>
         </div>
     </div>
+</div>
 <?php get_footer(); ?>
 
